2aba36ce7204ea920a853015105c5cbb5342b2ee
lvndry <lvndry@protonmail.com>
2022-03-14 20:00:24 +0100

    homepage styling

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,35 @@
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+	Links,
+	LiveReload,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
 } from "remix";
 import type { MetaFunction } from "remix";
+import styles from "~/styles/root.css";
 
 export const meta: MetaFunction = () => {
-  return { title: "AuditPhobia" };
+	return { title: "AuditPhobia" };
 };
 
+export const links = () => [{ rel: "stylesheet", href: styles }];
+
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <Scripts />
-        <ScrollRestoration />
-        <LiveReload />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width,initial-scale=1" />
+				<Meta />
+				<Links />
+			</head>
+			<body>
+				<Outlet />
+				<Scripts />
+				<ScrollRestoration />
+				{process.env.NODE_ENV === "development" && <LiveReload />}
+			</body>
+		</html>
+	);
 }
